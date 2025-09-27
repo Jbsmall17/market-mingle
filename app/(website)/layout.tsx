f64a192f44d4958f2b7532cb1fef65d330c96554
Ajibola <alaomiyaki17@gmail.com>
2025-09-27 07:53:09 +0100

    fixed deployment error 10

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,16 +1,18 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Newletter from '@/components/Newletter'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, Suspense } from 'react'
 
 export default function layout({children}:{children: ReactNode}) {
   return (
+    <Suspense fallback={<div>loading...</div>}>
     <main>
         <Header />
         {children}
         <Newletter />
         <Footer />
     </main>
+    </Suspense>
   )
 }
 
